cc14c214aed30c0bca344a4ffc0811c88970dd3d
Ian Videyko <ian.videyko@gmail.com>
2018-07-21 09:10:56 -0400

    another try at heroku mysql connect

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,10 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: "mysql://gkqt10anj6nrfnh1:lwhrzvugyy54e7hm@wiad5ra41q8129zn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/atxhjw5zd2yr7s71",
+  // host: 
+  
+  "mysql://gkqt10anj6nrfnh1:lwhrzvugyy54e7hm@wiad5ra41q8129zn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/atxhjw5zd2yr7s71"
+
   // port: 3306,
   // user: "root",
   // password: "root",
